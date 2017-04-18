2e0649d7b4ba556a7ad3b0c588282c6b66a76005
Louis Gates <louis@10footwave.com>
2017-04-18 09:16:42 -0400

    Changes to support Amazon SNS

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,7 +3,8 @@ class WebhookController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def callback
-    logger.debug response.body
+    logger.debug "RESPONSE: #{response.body}"
+    logger.debug "AMAZON MESSAGE TYPE: #{response['x-amz-sns-message-type']}"
     render :json => { data: "Success!" }
   end
   
